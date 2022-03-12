8284f53025dd8a3348ed46039546ff83e9c5b1b2
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-03-11 22:41:02 -0300

    Update favicon

--- components/metadata/header.php
+++ components/metadata/header.php
@@ -4,21 +4,22 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 	<!-- assets/favicon -->
-	<link rel="apple-touch-icon" sizes="57x57" href="./assets/favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="./assets/favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="./assets/favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="./assets/favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="./assets/favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="./assets/favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="./assets/favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="./assets/favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="./assets/favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="./assets/favicon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="./assets/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon-16x16.png">
-	<link rel="manifest" href="./assets/favicon/manifest.json">
-	<meta name="msapplication-TileImage" content="./assets/favicon/ms-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/favicon-16x16.png">
+	<link rel="manifest" href="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/manifest.json">
+	<meta name="msapplication-TileImage" content="HTTPS://rodcordeiro.github.io/shares/favicons/favicon-raizes/ms-icon-144x144.png">
+
 
 	<!-- LINKS -->
 	<link rel="stylesheet" type="text/css" href="./assets/css/mobile.css">
