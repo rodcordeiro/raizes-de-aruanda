5a5534cc38bd96a60bf1376fabfbd573e78b90a3
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-04-20 22:53:07 -0300

    Update header.php

--- components/metadata/header.php
+++ components/metadata/header.php
@@ -1,4 +1,4 @@
-	<title>Pontos de umbanda | ICNT</title>
+	<title>Pontos de umbanda | Raizes</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
