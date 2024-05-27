f48e091811c00f3f8558b5a6d1c4e022acc20877
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-05-27 12:12:06 -0300

    Update dash.php

--- dash.php
+++ dash.php
@@ -1,8 +1,10 @@
-function Redirect($url, $permanent = false)
-{
-    header('Location: ' . $url, true, $permanent ? 301 : 302);
-
-    die();
-}
-
-Redirect('http://82.180.136.148:3339/public-dashboards/2c77f4374db34cbd8344fd56e6951e6d', false);
+<?php
+    function Redirect($url, $permanent = false)
+    {
+        header('Location: ' . $url, true, $permanent ? 301 : 302);
+    
+        die();
+    }
+    
+    Redirect('http://82.180.136.148:3339/public-dashboards/2c77f4374db34cbd8344fd56e6951e6d', false);
+?>
